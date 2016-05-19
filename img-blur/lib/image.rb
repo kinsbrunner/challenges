7501c8d6a0256521f7904dccf302c172a8c78cd9
Alex Kinsbrunner <kinsbrunner@gmail.com>
2016-05-19 19:11:46 -0300

    Add files via upload

---
+++ img-blur/lib/image.rb
@@ -0,0 +1,46 @@
+class Image
+	attr_accessor :data, :rows, :cols
+	
+	def initialize(vals)
+		@data = vals
+		@rows = @data.count
+		@cols = @data[0].count
+	end
+	
+	def output_image
+		@data.each do |line|
+			puts line.join(" ")
+		end
+	end
+	
+	def blur(n = 1)
+		
+		n.times do
+			on_pixels = []
+			@data.each_index do |y|
+				subarray = @data[y]
+				subarray.each_index do |x|
+					on_pixels.push([y, x]) if @data[y][x] == 1
+				end
+			end
+
+			on_pixels.each do |position|
+				y, x = position
+				change_vals(y, x)
+			end
+		end
+	end
+	
+	private
+	
+	def change_vals(row, col)
+		#Left
+		@data[row][col-1] = 1 if col-1 >= 0
+		#Right
+		@data[row][col+1] = 1	if col+1 <= @cols - 1
+		#Up
+		@data[row-1][col] = 1 if row-1 >= 0
+		#Down
+		@data[row+1][col] = 1 if row+1 <= @rows - 1
+	end
+end
---
+++ img-blur/spec/image_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+RSpec.describe Image, type: :model do
+  describe "blurring should work" do
+    it "should blur a standard image" do
+      input = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ]
+
+      expected = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [1, 1, 1, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0]
+      ]
+     image = Image.new(input)
+     image.blur()
+     expect(image.data).to eq expected
+    end
+    
+    it "should blur a 2-pixel image" do
+      input = [
+        [0, 0, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ]
+
+      expected = [
+        [0, 0, 1, 0],
+        [0, 1,1, 1],
+        [0, 1, 1, 0],
+        [1, 1, 1, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0]
+      ]
+     image = Image.new(input)
+     image.blur()
+     expect(image.data).to eq expected
+    end 
+    
+    it "should blur a pixel-edge image" do
+      input = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ]
+
+      expected = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 0, 0, 0],
+        [1, 1, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 0, 0]
+      ]
+     image = Image.new(input)
+     image.blur()
+     expect(image.data).to eq expected
+    end     
+  end
+end
