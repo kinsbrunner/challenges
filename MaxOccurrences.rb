46fd66f7bc1699af432a79c91e2dd42a85c8acfd
kinsbrunner <kinsbrunner@gmail.com>
2016-08-29 20:11:28 -0400

    Added fix and alternative to MaxOccurrences

--- MaxOccurrences.rb
+++ MaxOccurrences.rb
@@ -4,8 +4,10 @@ def max_occ(arr)
   qty = 0
   num = nil
   freq.each do |key, value|
-    qty = value if value > qty || (value == qty && num > key)
-    num = key   if value > qty || (value == qty && num > key)
+    if value > qty || (value == qty && num > key) 
+      qty = value 
+      num = key
+    end
   end
   resp = Hash.new
   resp[:number] = num
@@ -13,5 +15,12 @@ def max_occ(arr)
   return resp
 end
 
+def mode3(arr) #alternative way to get most common numbers
+  freq = arr.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  max = freq.values.max                   # we're only interested in the key(s) with the highest frequency
+  freq.select { |k, f| f == max }         # extract the keys that have the max frequency
+end
+
 arr = [3, 3, 1, 1, 1, 2, 3, 1, 3]
 puts max_occ(arr).inspect
+puts mode3(arr).inspect
